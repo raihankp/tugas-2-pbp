c8c9e487abd877c78155a514acd8f51276a64117
raihan.kus <raihan.kus@ui.ac.id>
2022-09-14 21:55:37 +0700

    Mengerjakan Tugas2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_barang_katalog,
+        'name': 'Raihan Kus Putranto',
+        'id': '2106703821'
+    }
+    return render(request, "katalog.html", context)
